5411499159b2d018643bc0dafc5914ff75162445
jchapar <chaparrojcjr@gmail.com>
2023-02-02 21:01:58 -0700

    nav and heading - hero section markup and initial styling done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        teal: '#4d96a9',
+        purple: '#855fb1',
+        dark: '#28283d',
+        grey: '#87879d',
+        lightBlue: '#8fe3f9',
+        lightPurple: '#d9b8ff',
+      },
+      fontFamily: {
+        sans: ['Red Hat Display', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
